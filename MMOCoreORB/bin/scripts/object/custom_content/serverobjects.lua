693a68c6f7a62b4b023c9bd57465df07f1604637
Han Solo <pressinger.gene66@gmail.com>
2016-04-03 17:46:51 -0400

    Added CU and NGE weapons

--- MMOCoreORB/bin/scripts/object/custom_content/serverobjects.lua
+++ MMOCoreORB/bin/scripts/object/custom_content/serverobjects.lua
@@ -8,3 +8,11 @@ includeFile("custom_content/intangible/serverobjects.lua")
 includeFile("custom_content/mobile/serverobjects.lua")
 includeFile("custom_content/tangible/serverobjects.lua")
 includeFile("custom_content/weapon/serverobjects.lua")
+
+-- Server Objects
+--includeFile("custom_content/new.lua")
+
+
+
+
+
