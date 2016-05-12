389bf342fca28fecf8babdea21a84849164df545
KV90 <kaarvenix@yahoo.com>
2016-05-11 21:04:56 -0400

    Corrections
    To loot and Dant WB

--- MMOCoreORB/bin/scripts/mobile/custom_npcs/world_boss/world_boss_dantooine.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_npcs/world_boss/world_boss_dantooine.lua
@@ -7,8 +7,8 @@ world_boss_dantooine = Creature:new {
 	damageMin = 645,
 	damageMax = 1500,
 	baseXp = 55000,
-	baseHAM = 1106000,
-	baseHAMmax = 1352000,
+	baseHAM = 3106000,
+	baseHAMmax = 3352000,
 	armor = 3,
 	resists = {95,95,95,95,95,95,95,95,-1},
 	meatType = "",
@@ -29,19 +29,26 @@ world_boss_dantooine = Creature:new {
 		"object/mobile/dressed_dark_jedi_human_male_01.iff",
 		"object/mobile/dressed_dark_jedi_human_female_01.iff"},
 	lootGroups = {
-				{group = "holocron_dark", chance = 5000000},
-				{group = "holocron_light", chance = 5000000},
-				{group = "crystals_premium", chance = 5000000},
-				{group = "pearls_flawless", chance = 5000000},
-				{group = "rifles", chance = 1500000},
-				{group = "pistols", chance = 1500000},
-				{group = "melee_weapons", chance = 1500000},
-				{group = "armor_attachments", chance = 10000000},
-				{group = "clothing_attachments", chance = 10000000},
-				{group = "carbines", chance = 1500000},
-				{group = "dark_jedi_common", chance = 800000},
-				{group = "crystals_ancient", chance = 100000}
-				},
+		{
+		groups = {
+				{group = "holocron_dark", chance = 2500000},
+				{group = "holocron_light", chance = 2500000},
+				{group = "pearls_flawless", chance = 2500000},
+				{group = "rifles", chance = 500000},
+				{group = "pistols", chance = 500000},
+				{group = "melee_weapons", chance = 1000000},
+				{group = "carbines", chance = 500000},
+				}
+		},
+		{
+		groups = {
+		{group = "armor_attachments", chance = 4400000},
+		{group = "clothing_attachments", chance = 4500000},
+		{group = "crystals_ancient", chance = 100000}
+			},
+			lootChance = 10000000
+		}
+	},
 	weapons = {"dark_jedi_weapons_gen4"},
 	conversationTemplate = "",
 	attacks = merge(lightsabermaster,forcepowermaster)
