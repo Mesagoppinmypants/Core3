89e2ec70d524ae360abbda12176994ee7447d716
KV90 <kaarvenix@yahoo.com>
2016-05-21 11:53:05 -0400

    Fixes
    Fixes to force choke, melee2hArea3, named color crystals and dat damn
    1hr create timer.

---
+++ MMOCoreORB/bin/scripts/loot/groups/named_color_crystals.lua
@@ -0,0 +1,33 @@
+named_color_crystals = 
+{
+	description = "",
+	minimumlevel =0,
+	maximumLevel = -1,
+	lootitems = 
+				{
+				{itemTemplate = "force_crystal_bass_wisdom", chance = 500000},
+				{itemTemplate = "force_crystal_banes_heart", chance = 500000},
+				{itemTemplate = "force_crystal_bnars_sac", chance = 500000},
+				{itemTemplate = "force_crystal_mauls_vengence", chance = 500000},
+				{itemTemplate = "force_crystal_kuns_blood", chance = 500000},
+				{itemTemplate = "force_crystal_bondaras_folly", chance = 500000},
+				{itemTemplate = "force_crystal_ulics_redemption", chance = 500000},
+				{itemTemplate = "force_crystal_gallias_intuition", chance = 500000},
+				{itemTemplate = "force_crystal_prowess_of_plo_koon", chance = 500000},
+				{itemTemplate = "force_crystal_windus_guile", chance = 500000},
+				{itemTemplate = "force_crystal_mundis_response ", chance = 500000},
+				{itemTemplate = "force_crystal_sunriders_destiny ", chance = 500000},
+				{itemTemplate = "force_crystal_strength of_luminaria", chance = 500000},
+				{itemTemplate = "force_crystal_quintessence_of_the_force", chance = 500000},
+				{itemTemplate = "force_crystal_dawn_of_dagobah", chance = 1000000},
+				{itemTemplate = "force_crystal_kits_ferocity ", chance = 500000},
+				{itemTemplate = "force_crystal_horns_future ", chance = 500000},
+				{itemTemplate = "force_crystal_kenobis_legacy ", chance = 500000},
+				{itemTemplate = "force_crystal_qui_gons_devotion", chance = 500000}
+				--{itemTemplate = "crystal_lava", chance = 450000}
+				--{itemTemplate = "crystal_black_wing", chance = 450000}
+				--{itemTemplate = "crystal_perma_frost", chance = 450000}
+				}
+
+}
+addLootGroupTemplet("named_color_crystals", named_color_crystals)
